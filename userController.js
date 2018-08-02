3e47f2a4ed0c3a6869f068cf8daba420b85eec79
devjohnsonGA <dseth.johnson@gmail.com>
2018-08-02 11:35:03 -0400

    set up

---
+++ userController.js
@@ -0,0 +1,8 @@
+// /userController.js
+app.controller("userController", function($scope, UserF){
+    $scope.users = [];
+    UserF.getUsers()
+        .then(function(res) {
+            $scope.users = res.data.data;
+        })
+});
